a470fbda75ee80aa688c47813d9e7d405e095267
DavidEliass <jogadorbr602@gmail.com>
2020-12-18 16:21:01 -0300

    Finalizado Canvas

---
+++ APP/Public/Vendor/js/script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const tela = document.querySelector('#canvasDraw');
+  const context = tela.getContext('2d');
+
+  const pen = {
+    active: false,
+    move: false,
+    pos: { x: 0, y: 0 },
+    posPrevious: null,
+  };
+
+  // Defini o tamanho do canvas
+  tela.width = 1378;
+  tela.height = 671;
+
+  const drawLine = (line) => {
+    context.beginPath();
+    context.moveTo(line.posPrevious.x, line.posPrevious.y);
+    context.lineTo(line.pos.x, line.pos.y);
+
+    context.stroke();
+  };
+
+  tela.onmousedown = (event) => { pen.active = true; };
+  tela.onmouseup = (event) => { pen.active = false; };
+
+  tela.mouseleave = (event) => {pen.active = true}; 
+
+  tela.onmousemove = (event) => {
+    pen.pos.x = event.clientX;
+    pen.pos.y = event.clientY;
+    pen.move = true;
+  };
+
+  const cycle = () => {
+    if (pen.active && pen.move && pen.posPrevious) {
+      drawLine({ pos: pen.pos, posPrevious: pen.posPrevious });
+      pen.move = false;
+    }
+
+    pen.posPrevious = { x: pen.pos.x, y: pen.pos.y };
+
+    setTimeout(cycle, 10);
+  };
+
+  cycle();
+  // drawLine({ pos: { x: 350, y: 250 }, posPrevious: { x: 10, y: 20 } });
+});
--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const server = app.listen(3333, () => {
+  console.log('server running!');
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log('Nova Conexão');
+});
+
 app.use(express.static('./APP/Public'));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
-});
-
-app.listen(3333, () => {
-  console.log('Servidor rodando!');
 });
